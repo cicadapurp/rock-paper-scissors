243baf8cd3bcddf40fb415163658db63b980ef23
tallan groberg <tallan.taven@gmail.com>
2019-05-28 16:44:20 -0600

    click function for chose

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 
 
-console.log("test")
+
 var options = ["rock", "paper", "scissors"]
 var userChoice;
-var computerchoice
-var choices = document.getElementByClassName("choice");
-console.log(choices);
+var computerchoice = Math.random;
+var selected = document.getElementByTagName('choice');
 
 
-for(var i = 0; i < choices.length; i++) {
-  choices[i].addEventListener("click", function() {
-    userChoice = this.id;
+
+
+
     //display choices
     //get the comp choice figure out winner
     //figure out winner
-  });
-}
