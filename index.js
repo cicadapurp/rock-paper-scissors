7cb3deeb67d818708e682715b206d0562f9c8244
tallan groberg <tallan.taven@gmail.com>
2019-05-29 07:43:29 -0600

    basic game logic

--- index.js
+++ index.js
@@ -12,11 +12,21 @@ for(var i = 0; i < choices.length; i++) {
     userChoice = this.id;
     document.getElementById('computer').innerHTML = "the computer picked " + computerChoice;
     compare = userChoice + computerChoice;
-    if (userChoice === computerChoice) {
-      alert('its a tie')
-    } else {
-        alert("can find winner")
-    }
+    if (userChoice === 'paper' && computerChoice === 'rock') {
+    alert("player wins!")
+  } else if (userChoice === 'rock' && computerChoice === 'scissors'){
+    alert("player wins!")
+  } else if (userChoice === 'scissors' && computerChoice === 'paper'){
+    alert("player wins!")
+  } else if (computerChoice === 'paper' && userChoice === 'rock') {
+    alert("computer wins")
+  } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+    alert("computer wins")
+  } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+    alert("computer wins")
+  } else if (computerChoice === userChoice) {
+    alert("its a tie")
+  };
   });
 }
 
