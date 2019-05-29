c4aac419f3e8679cdede6c6c5c8dee83a87e2b49
tallan groberg <tallan.taven@gmail.com>
2019-05-29 08:19:35 -0600

    refactor alerts to innerHTML

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var computerChoice;
 var options = ["rock", "paper", "scissors"]
 var userChoice;
 var compare
+var display = document.getElementById('display')
 var choices = document.getElementsByClassName("choice");
 choices.innerHTML;
 computerChoice = options[Math.floor(Math.random() * options.length)];
@@ -11,21 +12,21 @@ for(var i = 0; i < choices.length; i++) {
   choices[i].addEventListener("click", function() {
     userChoice = this.id;
     document.getElementById('computer').innerHTML = "the computer picked " + computerChoice;
-    compare = userChoice + computerChoice;
+    compare = userChoice + computerChoice
     if (userChoice === 'paper' && computerChoice === 'rock') {
-    alert("player wins!")
+      display.innerHTML = 'player wins!'
   } else if (userChoice === 'rock' && computerChoice === 'scissors'){
-    alert("player wins!")
+    display.innerHTML = "player wins!"
   } else if (userChoice === 'scissors' && computerChoice === 'paper'){
-    alert("player wins!")
+    display.innerHTML = "player wins!"
   } else if (computerChoice === 'paper' && userChoice === 'rock') {
-    alert("computer wins")
+    display.innerHTML("computer wins")
   } else if (computerChoice === 'rock' && userChoice === 'scissors') {
-    alert("computer wins")
+    display.innerHTML = "computer wins"
   } else if (computerChoice === 'scissors' && userChoice === 'paper') {
-    alert("computer wins")
+    display.innerHTML = "computer wins"
   } else if (computerChoice === userChoice) {
-    alert("its a tie")
+    display.innerHTML = "its a tie"
   };
   });
 }
