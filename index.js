f4053eca7f2cb8db22765c20f9ccc445b2be25aa
tallan groberg <tallan.taven@gmail.com>
2019-05-29 07:14:34 -0600

    basic game logic

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 
-
-
+var computerChoice;
 var options = ["rock", "paper", "scissors"]
 var userChoice;
-var computerchoice = Math.random;
-var selected = document.getElementByTagName('choice');
+var compare
+var choices = document.getElementsByClassName("choice");
+choices.innerHTML;
+computerChoice = options[Math.floor(Math.random() * options.length)];
+
+for(var i = 0; i < choices.length; i++) {
+  choices[i].addEventListener("click", function() {
+    userChoice = this.id;
+    document.getElementById('computer').innerHTML = "the computer picked " + computerChoice;
+    compare = userChoice + computerChoice;
+    if (userChoice === computerChoice) {
+      alert('its a tie')
+    } else {
+        alert("can find winner")
+    }
+  });
+}
 
 
 
 
-
-    //display choices
-    //get the comp choice figure out winner
-    //figure out winner
+console.log(userChoice)
